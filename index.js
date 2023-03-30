0b04af8b928e659b26ebc86cf9d2f7152f7280e4
HedShat <ivo_argentina_07@hotmail.com>
2023-03-30 03:35:32 -0300

    a

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const app = express();
 //app.use(express.urlencoded({ extended: false }));
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://dartearteback.up.railway.app",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
