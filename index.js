4234151a76727e35bf02478de320ba6a941f9106
HedShat <ivo_argentina_07@hotmail.com>
2023-03-30 15:28:25 -0300

    a

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ import session from "express-session";
 import mysql from "mysql";
 import routerUserCompra from "./routes/users/routesCompra.js";
 import routerUserPedidos from "./routes/users/routesPedido.js";
-
+const PORT = process.env.PORT || 8080;
 //import bcrypt from "bcrypt";
 //const saltRounds = 10;
 //import jwt from "jsonwebtoken";
@@ -33,7 +33,6 @@ const corsOptions = {
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
-const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json({ limit: 52428800 }));
